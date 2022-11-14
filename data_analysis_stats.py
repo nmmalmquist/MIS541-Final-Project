10e3475849d2fb4b0c216216c009152834c4c95d
nmmalmquist <nicholasmmalmquist@gmail.com>
2022-11-13 21:09:02 -0600

    .

---
+++ data_analysis_stats.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+
+def main():
+
+    sec_data = pd.read_csv("./merged_data/SEC Schools.csv")
+    arts_data = pd.read_csv("./merged_data/Liberal Arts Schools.csv")
+
+    
+
+
+
+
+
+
+
+
+
+
+main()
---
+++ test.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import gzip
+import re
+import matplotlib.pyplot as plt
+import numpy as np
+from textblob import TextBlob
+from scipy import stats
+from statsmodels.formula.api import ols
+from statsmodels.formula.api import logit
+from statsmodels.formula.api import mnlogit
+from nltk.corpus import stopwords
+import math
+from wordcloud import STOPWORDS
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
